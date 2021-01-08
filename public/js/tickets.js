5069f8c1268a4726fc7d4e427aef97ccbca2f837
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2021-01-08 22:00:27 +0000

    Incorporated new pages and usertype for events.

--- public/js/tickets.js
+++ public/js/tickets.js
@@ -1,19 +1,3 @@
-const eventList = document.querySelector('.events');
-const setupEvents = (data) => {
-    let html = '';
-    data.forEach(doc => {
-        const event = doc.data();
-        const li = `
-            <div class="col-lg-4 col-md-6 col-xs-12 text-center">
-                <h3>${event.title}</h3>
-                <p>${event.content}</p>
-            </div>
-        `;
-        html += li;
-    });
-    eventList.innerHTML = html;
-}
-
 //searches HTML file for items with the class of 'logged-out' and 'logged-in'
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
@@ -34,32 +18,14 @@ const setupUI = (user) => {
     }
 }
 
-const createForm = document.querySelector('#create-form');
-createForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('events').add({
-        title: createForm.title.value,
-        content: createForm.content.value,
-        userType: accountType
-    }).then(() => {
-        //close modal and reset form
-        $('#event-modal').modal('hide');
-        createForm.reset();
-    }).catch(err => {
-        console.log(err.message);
-    });  
-});
-
 //checks that the sign in status has changed before calling setupUI
 auth.onAuthStateChanged(user => {
     if (user) {
         //get data
         db.collection('events').onSnapshot(snapshot => {
-            setupEvents(snapshot.docs);
             setupUI(user);
         });
     } else {
         setupUI();
-        setupEvents([]);
     }
 });
