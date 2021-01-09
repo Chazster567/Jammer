b8a911acde4562e3da36ec49b62cf19ae0cd72f5
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2021-01-09 17:05:14 +0000

    Integrated new pages and forms.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,23 @@
 const eventList = document.querySelector('.events');
+//searches HTML file for items with the class of 'logged-out' and 'logged-in'
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+var accountType;
+
+//changes the display style of the navbar elements based of the sign in status
+const setupUI = (user) => {
+    if (user) {
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+    } else {
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block');
+    }
+}
+
 const setupEvents = (data) => {
     let html = '';
-    data.forEach(doc => {
+    data.forEach (doc => {
         const event = doc.data();
         const li = `
             <div class="col-lg-4 col-md-6 col-xs-12 text-center">
@@ -12,26 +28,6 @@ const setupEvents = (data) => {
         html += li;
     });
     eventList.innerHTML = html;
-}
-
-//searches HTML file for items with the class of 'logged-out' and 'logged-in'
-const loggedOutLinks = document.querySelectorAll('.logged-out');
-const loggedInLinks = document.querySelectorAll('.logged-in');
-var accountType;
-
-//changes the display style of the navbar elements based of the sign in status
-const setupUI = (user) => {
-    if (user) {
-        db.collection('users').doc(user.uid).get().then(doc => {
-            accountType = doc.data().userType;
-        });
-        
-        loggedInLinks.forEach(item => item.style.display = 'block');
-        loggedOutLinks.forEach(item => item.style.display = 'none');
-    } else {
-        loggedInLinks.forEach(item => item.style.display = 'none');
-        loggedOutLinks.forEach(item => item.style.display = 'block');
-    }
 }
 
 const createForm = document.querySelector('#create-form');
@@ -53,13 +49,19 @@ createForm.addEventListener('submit', (e) => {
 //checks that the sign in status has changed before calling setupUI
 auth.onAuthStateChanged(user => {
     if (user) {
-        //get data
-        db.collection('events').onSnapshot(snapshot => {
-            setupEvents(snapshot.docs);
-            setupUI(user);
+        db.collection('users').doc(user.uid).get().then(doc => {
+            //pulls in the user type for filtering purposes
+            accountType = doc.data().userType;
+
+            //only pulls in events that apply to the user type
+            db.collection('events').where('userType', '!=', accountType).onSnapshot(snapshot => {
+                setupEvents(snapshot.docs);
+                setupUI(user);
+            });
         });
     } else {
         setupUI();
         setupEvents([]);
     }
 });
+
