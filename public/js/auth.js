f89ffea2b23711351dc666d099385205e3282381
Chazster567 <charles.whitehair@students.plymouth.ac.uk>
2021-01-10 19:59:13 +0000

    Added profile images to events

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,10 @@
 //searches HTML file for items with the ID of 'signup-form'
-const signupForm = document.querySelector('#signup-form')
+const signupForm = document.querySelector('#signup-form');
+let file = {};
+
+function chooseFile(pfp) {
+    file = pfp.target.files[0];
+}
 
 //adds user to database
 signupForm.addEventListener('submit', (e) => {
@@ -12,6 +17,12 @@ signupForm.addEventListener('submit', (e) => {
 
     //firebase function adds user to database
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        firebase.storage().ref('users/' + cred.user.uid + '/profile.png').put(file).then(function() {
+            console.log('successfully uploaded');
+        }).catch(error => {
+            console.log(error.message);
+        });
+
         return db.collection('users').doc(cred.user.uid).set({
             displayName: signupForm['inputUsername'].value,
             userType: signupForm['userType'].value
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
 const eventList = document.querySelector('.events');
+const myEventList = document.querySelector('.myEvents');
 //searches HTML file for items with the class of 'logged-out' and 'logged-in'
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 var accountType;
+var userName;
+var img;
 
 //changes the display style of the navbar elements based of the sign in status
 const setupUI = (user) => {
@@ -18,16 +21,35 @@ const setupUI = (user) => {
 const setupEvents = (data) => {
     let html = '';
     data.forEach (doc => {
-        const event = doc.data();
+        const eventCurrent = doc.data();
         const li = `
             <div class="col-lg-4 col-md-6 col-xs-12 text-center">
-                <h3>${event.title}</h3>
-                <p>${event.content}</p>
+                <img src='${eventCurrent.pfp}' style="width: 50%; border-radius: 50%;">
+                <h3 style="padding-top: 1rem;">${eventCurrent.title}</h3>
+                <p style="margin-bottom: 5px;">${eventCurrent.content}</p>
+                <p style="font-weight: bold;">${eventCurrent.userType}</p>
             </div>
         `;
         html += li;
     });
     eventList.innerHTML = html;
+}
+
+const setupMyEvents = (data) => {
+    let html = '';
+    data.forEach (doc => {
+        const eventCurrent = doc.data();
+        const li = `
+            <div class="col-lg-4 col-md-6 col-xs-12 text-center">
+                <img src='${eventCurrent.pfp}' style="width: 50%; border-radius: 50%;">
+                <h3 style="padding-top: 1rem;">${eventCurrent.title}</h3>
+                <p style="margin-bottom: 5px;">${eventCurrent.content}</p>
+                <p style="font-weight: bold;">${eventCurrent.userType}</p> 
+            </div>
+        `;
+        html += li;
+    });
+    myEventList.innerHTML = html;
 }
 
 const createForm = document.querySelector('#create-form');
@@ -36,7 +58,9 @@ createForm.addEventListener('submit', (e) => {
     db.collection('events').add({
         title: createForm.title.value,
         content: createForm.content.value,
-        userType: accountType
+        userType: accountType,
+        userID: userName,
+        pfp: img
     }).then(() => {
         //close modal and reset form
         $('#event-modal').modal('hide');
@@ -52,16 +76,25 @@ auth.onAuthStateChanged(user => {
         db.collection('users').doc(user.uid).get().then(doc => {
             //pulls in the user type for filtering purposes
             accountType = doc.data().userType;
+            userName = user.uid;
 
             //only pulls in events that apply to the user type
             db.collection('events').where('userType', '!=', accountType).onSnapshot(snapshot => {
                 setupEvents(snapshot.docs);
                 setupUI(user);
             });
+            db.collection('events').where('userID', '==', userName).onSnapshot(snapshot => {
+                setupMyEvents(snapshot.docs);
+            });
+        });
+
+        firebase.storage().ref('users/' + user.uid + '/profile.png').getDownloadURL().then(imgUrl => {
+            img = imgUrl;
         });
     } else {
         setupUI();
         setupEvents([]);
+        setupMyEvents([]);
     }
 });
 
